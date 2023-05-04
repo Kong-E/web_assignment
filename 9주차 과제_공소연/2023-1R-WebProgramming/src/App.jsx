c62621acbf21d5c97c110ad8fd7be9a86665dc94
Kong-E <kng001016@dgu.ac.kr>
2023-05-04 23:10:54 +0900

    feat : 서울시 미세먼지 데이터 조회

--- 9주차 과제_공소연/2023-1R-WebProgramming/src/App.jsx
+++ 9주차 과제_공소연/2023-1R-WebProgramming/src/App.jsx
@@ -1,22 +1,50 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [row, setRow] = useState([]);
+  const onLoad = () => {
+    fetch(
+      "http://openapi.seoul.go.kr:8088/72534779436772613635674d596a53/json/RealtimeCityAir/1/25"
+    )
+      .then((res) => res.json())
+      .then((res) => setRow(res.RealtimeCityAir.row));
+  };
 
-  const newLocal = "https://vitejs.dev";
+  console.log(row);
+
   return (
     <>
-      <div>
+      {/* MSRRGN_NM, O3, PM10, IDEX_NM */}
+      <button onClick={onLoad}>불러오기</button>
+      <table>
+        <thead>
+          <tr>
+            <th>MSRRGN_NM</th>
+            <th>O3</th>
+            <th>PM10</th>
+            <th>IDEX_NM</th>
+          </tr>
+        </thead>
+        <tbody>
+          {row.map((item) => (
+            <tr key={item.MSRSTE_NM}>
+              <td>{item.MSRRGN_NM}</td>
+              <td>{item.O3}</td>
+              <td>{item.PM10}</td>
+              <td>{item.IDEX_NM}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {/* <div>
         <a href={newLocal} target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <a href="https://react.d`ev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div>`
       <h1>Vite + Resort</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,7 +56,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   );
 }
